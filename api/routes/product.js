0ca00381ba78a4e7b0608748877e03fbc36e61dd
Alexrmsu <al.magna@alumnos.duoc.cl>
2023-04-23 01:37:31 -0400

    product route add

--- api/routes/product.js
+++ api/routes/product.js
@@ -14,4 +14,18 @@ router.get('/', (req, res) => {
     })
 })
 
+router.post('/add', verifyToken, (req, res) => {
+    const { name, price, description, image } = req.body;
+    connection.query('INSERT INTO products SET ?', { name, price, description, image }, (err, rows, fields) => {
+        if (!err) {
+            res.json({ status: 'Product saved' });
+        } else {
+            console.log(err);
+        }
+    })
+})
+
+
+
+
 module.exports = router;
